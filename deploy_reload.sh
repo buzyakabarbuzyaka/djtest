d12f5fdb312d18961ec3386c197e6d89e6d32769
Buzov Vyahceslav <buzovv1997@gmail.com>
2019-11-25 14:42:38 +0300

    deploy script test fix 1

--- deploy_reload.sh
+++ deploy_reload.sh
@@ -2,7 +2,7 @@
 
 sudo fuser 8000/tcp -k
 
-cd djtest
-sudo git pull
-pip3 install -r requirements.txt
-nohup python3 manage.py runserver 0.0.0.0:8000
+cd /home/ark/djtest ; echo "1"
+sudo git pull ; echo "2"
+pip3 install -r requirements.txt ; echo "3"
+nohup python3 manage.py runserver 0.0.0.0:8000 ; echo "4"
