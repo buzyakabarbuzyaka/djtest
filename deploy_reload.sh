4380b749f41492d7632df3f63b06c98c6a95e97d
Buzov Vyahceslav <buzovv1997@gmail.com>
2019-11-26 14:41:56 +0300

    deploy script test fix 8

--- deploy_reload.sh
+++ deploy_reload.sh
@@ -4,6 +4,6 @@ sudo fuser 8000/tcp -k
 
 cd /home/ark/djtest
 sudo git pull
-pip3 install -r requirements.txt
 . venv/bin/activate
+pip3 install -r requirements.txt
 nohup python3 manage.py runserver 0.0.0.0:8000 > server_log.txt &
