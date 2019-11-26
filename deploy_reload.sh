b294f4255cc76af96a3cf531318bbc444dd58107
Buzov Vyahceslav <buzovv1997@gmail.com>
2019-11-26 20:00:47 +0300

    final deploy scripts and structure

--- deploy_reload.sh
+++ deploy_reload.sh
@@ -6,4 +6,4 @@ cd /home/ark/djtest
 sudo git pull
 . venv/bin/activate
 pip3 install -r requirements.txt
-nohup python3 manage.py runserver 0.0.0.0:8000 > server_log.txt &
+nohup python3 manage.py runserver 0.0.0.0:8000 > logs/server_log.txt &
