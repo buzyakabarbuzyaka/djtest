66e6277f4c0bc0504dc0fb6202ec333ccd2d61fc
Buzov Vyahceslav <buzovv1997@gmail.com>
2019-12-16 13:08:19 +0300

    добавлен ID для каждого экземпляра

--- jput/views.py
+++ jput/views.py
@@ -7,9 +7,10 @@ class JsonView(APIView):
     def get(self, request):
         return Response({"predicton": False})
 
+from django.conf import settings
 
 def hello_world(request):
     with open("./jput/templates/jput/dick_pic.txt", "r") as f:
         data = f.readlines()
     data = [l.strip() for l in data]
-    return render(request, 'jput/hello_world.html', {'data': data})
+    return render(request, 'jput/hello_world.html', {'data': data, 'ID': settings.ID})
--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#TODO: может быть не лучшее место для генерации ID
+
+import random
+ID = random.random()
